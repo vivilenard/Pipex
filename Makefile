df13b65e4b4ca7096165b4e00956ad659a3397d9
Vivien Lenard <vlenard@1-B-31.42heilbronn.de>
2023-01-23 19:50:21 +0100

    anfang

---
+++ Makefile
@@ -0,0 +1,33 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: vlenard <vlenard@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/10/15 15:04:49 by vlenard           #+#    #+#              #
+#    Updated: 2023/01/23 11:14:07 by vlenard          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = pipex
+CC = cc
+CFLAGS = -Wall -Werror -Wextra
+SRC = 	main.c
+OBJS = $(SRC:.c=.o)
+LIBFT = libft/libft.a
+
+all: $(NAME)
+$(NAME): $(LIBFT) $(OBJS)
+		$(CC) $(OBJS) $(LIBFT) -o $(NAME)
+$(LIBFT): 
+		cd libft && make && make clean
+n:
+		norminette $(SRC) $(NAME).h
+clean :
+		rm -f $(OBJS)
+fclean : clean
+		rm -f ./$(NAME)
+re : fclean
+		$(MAKE)
+.PHONY: all, n, clean, fclean, re, v, $(LIBFT), $(NAME)
---
+++ main.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlenard <vlenard@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 11:13:19 by vlenard           #+#    #+#             */
+/*   Updated: 2023/01/23 19:48:20 by vlenard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+char	*ft_searchbinary(char **env)
+{
+	int	i;
+
+	i = 0;
+	while (*env[i])
+	{
+			// ft_putstr_fd(env[i], 1);
+			// write (1, "\n", 1);
+		//ft_printf("%s\n", ft_haystack(env[i], "PATH"));
+
+		if (ft_haystack(env[i], "PATH") != NULL)
+			return (env[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+int ft_makemeachild(int *fdpipe, char **argv, char **env)
+{
+	int	pid1;
+
+	ft_printf("av: %s\n", argv[2]);
+	pid1 = fork();
+	if (pid1 == 0)
+	{
+		ft_printf("Path is: %s\n", ft_searchbinary(env));
+							ft_printf("PID1 = %d hi\n", pid1);
+		ft_printf("pid1 %d\n fdpipe %d\n", pid1, fdpipe[0]);
+	}
+	wait(NULL);
+	return (0);
+}
+
+
+int main(int argc, char **argv, char **env)
+{
+	int stdin;
+	int fd;
+	int	fdpipe[1];
+	int n;
+	
+	if (argc < 1)
+		return (0);
+	//replace stdin with file1
+	stdin = dup(STDIN_FILENO);
+	fd = open("file1", O_RDONLY);
+	fd = dup2(fd, STDIN_FILENO);
+	ft_printf("stdin: %d, file1: %d\n", stdin, fd);
+	
+	//search for 
+
+	//create pipe
+	if (pipe(fdpipe) == -1)
+		return (0);
+		
+	//fork
+	n = 2;
+	ft_makemeachild(fdpipe, argv, env);
+
+	//go back to normal
+	dup2(stdin, STDIN_FILENO);
+	close(fd);
+	return (0);
+}
+		//look for path of argv[n]
+		// pid2 = fork();
+		// if (pid2 == 0)
+		// {
+		// 			ft_printf("PID2 = %d hi\n", pid2);
+		// 			ft_printf("pipe: %d\n", fdpipe[0]);
+		// }
